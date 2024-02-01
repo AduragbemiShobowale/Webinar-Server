a1b8cc5815097fcc3dcea732918c80abb23115ba
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2024-02-01 15:57:54 +0100

    update: updated the node version required

--- utils/emailModule.js
+++ utils/emailModule.js
@@ -17,59 +17,12 @@ const confirmationEmail = async ({
   email,
 }) => {
   try {
-    const emailBody = `<div
-    style="
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-      margin: auto;
-    "
-  >
-    <div style="width: 38%; text-align: center">
-      <div
-        style="
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          color: white;
-          background-color: black;
-        "
-      >
-        <img src="./Group.svg" alt="" style="height: auto" />
-        <h3>The Ultimate Academy Launch</h3>
-      </div>
-      <div style="background-color: #f1f2fe; padding: 32px 0; color: black; display: flex; flex-direction: column; align-items: center; gap: 24px;">
-        <img
-          src="./Frame 1000003375.svg"
-          alt=""
-          style="display: block; margin: auto"
-        />
-        <h3 style="padding:0 86px">
-          Congratulations, ${firstName}!🎉 You're officially part of our
-          upcoming Exclusive Webinar Experience
-        </h3>
-        <p style="padding: 0 70px;">
-          Lorem ipsum dolor sit amet consectetur. Habitant aliquet suscipit
-          sed facilisi sit. Nibh at nisl augue viverra vitae amet orci lorem.
-          Luctus faucibus laoreet eu parturient in. Elementum consectetur enim
-          fames velit sit donec.
-        </p>
-        <a
-          href="https://meet.google.com/mio-jswu-gto"
-          style="
-            display: inline-block;
-            padding: 12px 150px;
-            background-color: #2f40d5;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 5px;
-          "
-          >Start Meeting</a
-        >
-      </div>
-    </div>
-  </div>`;
+    const emailBody = `<p style="padding: 0 70px;">
+    Lorem ipsum dolor sit amet consectetur. Habitant aliquet suscipit
+    sed facilisi sit. Nibh at nisl augue viverra vitae amet orci lorem.
+    Luctus faucibus laoreet eu parturient in. Elementum consectetur enim
+    fames velit sit donec.
+  </p>`;
 
     const info = await mailTransporter.sendMail({
       from: process.env.NODEMAIL_EMAIL,
